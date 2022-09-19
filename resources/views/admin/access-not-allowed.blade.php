51585c298b92ca5d82ad8471d1b40bb2246fa56a
ajbmerto <alvinjay.merto@minda.gov.ph>
2022-09-19 10:58:21 +0800

    access not allowed

--- resources/views/admin/access-not-allowed.blade.php
+++ resources/views/admin/access-not-allowed.blade.php
@@ -48,8 +48,8 @@ $(document).ready(function(e){
                                 <img src="{{ url('/images/icon-declined.png') }}" alt="1" width="auto" height="auto" align="center"><br>
                                 <span style="font-size: 18px; color: #6E6E6E;">Sorry, you do not have permission to view this page</span><br><br>
                                 <p style="color: #084B8A; font-weight: bold; font-size: 20px;">Mindanao Development Authority</p>
-                                <p style="color: #A4A4A4; font-weight: bold; font-family: Georgia, serif;">Old Airport Bldg., Old Airport Road, <br>
-                                Km. 9, Sasa, Davao City 8000 Philippines<br></p>
+                                <p style="color: #A4A4A4; font-weight: bold; font-family: Georgia, serif;">10th and 14th floor, Pryce Tower building, Bajada Street <br>
+                                Davao City 8000 Philippines<br></p>
                                 <p>www.minda.gov.ph<br><br></p>
                                 <p style="color: #A4A4A4;">Telefax No.: (082) 221-7060/(082) 221-6929</p>
                             </div>
