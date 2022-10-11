ca3c8d7cf5533d4deba1f3b10c9a5e4683c09407
ajbmerto <alvinjay.merto@minda.gov.ph>
2022-10-11 16:22:59 +0800

    made changes 10112022

--- resources/views/internal/doc-view-track-list.blade.php
+++ resources/views/internal/doc-view-track-list.blade.php
@@ -518,10 +518,12 @@ margin-top: 10px;
                                                                         $remarks = null;
 
                                                                         //if ( preg_match('/[\'\/~`\!@#\$%\^&\*\(\)_\-\+=\{\}\[\]\|;:"\<\>,\.\?\\\]/',$rems[count($rems)-1]) == 1) {
-                                                                        if ( $rems[count($rems)-1][0] != "*" ) {
-                                                                            $remarks = $rems[count($rems)-1];
-                                                                            unset($rems[count($rems)-1]);
-                                                                        } 
+                                                                        if (count($rems) > 0) {
+                                                                            if ( $rems[count($rems)-1][0] != "*" ) {
+                                                                                $remarks = $rems[count($rems)-1];
+                                                                                unset($rems[count($rems)-1]);
+                                                                            } 
+                                                                        }
 
                                                                         echo implode(" ",$rems);
                                                                     ?>
